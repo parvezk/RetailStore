13b7d4571bba5e121f76612aad53e28d7294669f
Parvez Kose <parvez.kosex@gmail.com>
2024-12-18 12:58:51 -0500

    add amplify

--- src/app/[[...slug]]/page.tsx
+++ src/app/[[...slug]]/page.tsx
@@ -1,9 +1,13 @@
-'use client'
-import React from 'react';
-import dynamic from 'next/dynamic';
+"use client";
+import React from "react";
+import dynamic from "next/dynamic";
+import { Amplify } from "aws-amplify";
+import config from "../../../amplify_outputs.json";
 
-const App = dynamic(() => import('../../App'), { ssr: false })
+Amplify.configure(config);
+
+const App = dynamic(() => import("../../App"), { ssr: false });
 
 export default function Page() {
-  return <App />
+  return <App />;
 }
